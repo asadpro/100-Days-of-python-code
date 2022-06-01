666fdbed6255fa16a0a7a00d4f79544cdf918cba
asadpro <asadjani202@gmail.com>
2022-06-01 13:00:45 +0500

    day 1 has been completed

---
+++ 1. Day 1 - Beginner - Working with Variables in Python to Manage Data.py
@@ -0,0 +1,53 @@
+# String Manipulatoin and code intelligence
+
+print('Day 1 - Python Print Function')
+print('The function is declared like this:')
+print("print(\"what to print\")")
+
+# The python input function
+
+print('Hello  m '+input("What is your name?\n"))
+
+# Playing with variables 
+# 🚨 Don't change the code below 👇
+print('Before switching values')
+a = input("a: ")
+b = input("b: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+
+# Long answer for switching values between a & b
+# new_a = b
+# new_b = a
+# a = new_a
+# b = new_b
+
+# short answer for switching values
+c = a
+a = b
+b = c
+
+
+
+#Write your code above this line 👆
+####################################
+
+# 🚨 Don't change the code below 👇
+print('\nAfter switching values')
+print("a: " + a)
+print("b: " + b)
+
+# Final Quiz of Day 1 Band name generator
+
+print('Welcome to the Band Name Generator.\n')
+city = input('What\'s name of the city you grew up in?\n')
+print(city)
+pet_name = input('What\'s your pets name?\n')
+print(f'Your band name could be: {pet_name + " "+  city}')
+
+
+
+
+  
