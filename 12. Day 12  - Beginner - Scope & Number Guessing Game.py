99fd2b0841cc98f6157049b544848ddbf6abd324
asadpro <asadjani202@gmail.com>
2022-07-05 08:49:07 +0500

    Day 13 has been completed successfully

---
+++ 12. Day 12  - Beginner - Scope & Number Guessing Game.py
@@ -0,0 +1,15 @@
+
+
+
+def increase_enemies():
+    def game():
+        enemies = 2
+        name = 'asad'
+        print(f'Enemies inside function {enemies}')
+
+print(f'Enemies outside function {enemies}')
+
+# try: 
+#     print(something) #"something" is not defined
+# except NameError:
+#     print('You have put the wrong variable name this variable is not defined yet....')
