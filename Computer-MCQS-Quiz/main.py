8de90b3ffc0a91165eb2e8a6c26a84508121ef0e
asadpro <asadjani202@gmail.com>
2022-08-23 13:39:48 +0500

    snake game completed of 20 & 21 day snake game completed of 20 & 21 day snake game completed of 20 & 21 day snake game completed of 20 & 21 day snake game completed of 20 & 21 day snake game completed of 20 & 21 day snake game completed of 20 & 21 day snake game completed of 20 & 21 day snake game completed of 20 & 21 day snake game completed of 20 & 21 day snake game completed of 20 & 21 day snake game completed of 20 & 21 day snake game completed of 20 & 21 day snake game completed of 20 & 21 day snake game completed of 20 & 21 day snake game completed of 20 & 21 day snake game completed of 20 & 21 day snake game completed of 20 & 21 day snake game completed of 20 & 21 day snake game completed of 20 & 21 day snake game completed of 20 & 21 day snake game completed of 20 & 21 day snake game completed of 20 & 21 day snake game completed of 20 & 21 day snake game completed of 20 & 21 day snake game completed of 20 & 21 day snake game completed of 20 & 21 day snake game completed of 20 & 21 day snake game completed of 20 & 21 day

--- Computer-MCQS-Quiz/main.py
+++ Computer-MCQS-Quiz/main.py
@@ -1,19 +1,26 @@
-import data
-from question_model import Question
-from quiz_hall import *
+# import data
+# from question_model import Question
+# from quiz_hall import *
 
-data_file = data.actual_data
+# data_file = data.actual_data
 
-question_bank = []
+# question_bank = []
 
-for data_item in data_file["results"]:
+# for data_item in data_file["results"]:
 
-    question_text = data_item["question"]
-    question_answer = data_item["correct_answer"]
-    new_question = Question(text=question_text, answer=question_answer)
-    question_bank.append(new_question)
+#     question_text = data_item["question"]
+#     question_answer = data_item["correct_answer"]
+#     new_question = Question(text=question_text, answer=question_answer)
+#     question_bank.append(new_question)
 
-quiz = Quiz(question_list=question_bank)
+# quiz = Quiz(question_list=question_bank)
 
-while quiz.still_has_question():
-    quiz.next_question()
+# while quiz.still_has_question():
+#     quiz.next_question()
+
+
+# if __name__ == '__main__':
+#     n = int(input())
+#     arr = map(int, input().split())
+#     print(n)
+
--- youtube_video_downloader.py
+++ youtube_video_downloader.py
@@ -7,46 +7,94 @@
 
 
 # # ⛵⛵⛵⛵⛵ Creating youtube video downloader
-# from pytube import YouTube
+from pytube import YouTube
 
-# youtube = YouTube("https://youtu.be/EAYlckSaviI")
+# youtube = YouTube("https://www.youtube.com/watch?v=OKuiyX5k6zg&list=WL&index=67&t=3410s&ab_channel=WsCubeTech")
+youtube = YouTube("https://www.youtube.com/watch?v=VPtG296lBbk&list=WL&index=71&t=632s&ab_channel=MemoonaMuslima")
+# youtube = YouTube("https://www.youtube.com/watch?v=8ext9G7xspg&t=16s&ab_channel=freeCodeCamp.org")
+# print(youtube.thumbnail_url)
 
-# # print(youtube.title)
-# # print(youtube.thumbnail_url)
-# # print(youtube.age_restricted)
-# # print(youtube.streams)
+# print(youtube.title)
+# print(youtube.thumbnail_url)
+# print(youtube.age_restricted)
+# print(youtube.streams)
 
-# all_streams = (
-#     youtube.streams.asc()
-# )  # Save the resolution streams in a variable in ascending order
+all_streams = (
+    youtube.streams.asc()
+)  # Save the resolution streams in a variable in ascending order
 
-# # The below code will only store those streams which are available in both audio & video format to download.
-# # all_streams = youtube.streams.filter(progressive=True)
+# The below code will only store those streams which are available in both audio and video format to download.
+# all_streams = youtube.streams.filter(progressive=True)
 
-# # Download by resolution.
-# # stream_2 = youtube.streams.get_by_resolution(resolution='720p')
+# Download by resolution.
+# stream_2 = youtube.streams.get_by_resolution(resolution='720p')
 
-# enumerate_stream = enumerate(
-#     all_streams, start=1
-# )  # Create an enumerate and start from 1.
-# list_stream = list(enumerate_stream)  # Convert enumerate into list.
+enumerate_stream = enumerate(
+    all_streams, start=1
+)  # Create an enumerate and start from 1.
+list_stream = list(enumerate_stream)  # Convert enumerate into list.
 
-# for count, item in list_stream:
-#     print(count, item)
+for count, item in list_stream:
+    print(count, item)
 
-# download = int(input("Resolution to download Video: "))
-# all_streams[download].download()
-# print("Successfully!!!")
+download = int(input("Resolution to download Video: "))
+all_streams[download].download()
+print("Successfully!!!")
 
 # 🌎🌎🌎🌎🌎 Python playlist downloader 🌎🌎🌎🌎🌎
-from pytube import Playlist
+# from pytube import Playlist
 
-playlist = Playlist(
-    "https://www.youtube.com/watch?v=Gj6V-xZgtlQ&list=PLRza9ng-gU7xKHgkttUffXAkvhrv_ydqf"
-)
+# playlist = Playlist(
+#     "https://www.youtube.com/watch?v=Gj6V-xZgtlQandlist=PLRza9ng-gU7xKHgkttUffXAkvhrv_ydqf"
+# )
 
-print(playlist.title)
+# print(playlist.title)
 
-# In order to download full playlist we need to iterate through every video inside playlist and download them one by one using for loop.
-for video in playlist.videos[0:2]:
-    video.streams.first().download()
+# # In order to download full playlist we need to iterate through every video inside playlist and download them one by one using for loop.
+# for video in playlist.videos[0:2]:
+#     video.streams.first().download()
+
+# from pytube import YouTube
+
+# python_project = YouTube('https://www.youtube.com/watch?v=8ext9G7xspgandt=16s',on_complete_callback="Download has been completed!!!")
+
+# stream_options = python_project.streams.get_highest_resolution()
+
+# stream_options.download()
+
+# x = int(input()) #1
+# y = int(input()) #1
+# z = int(input()) #1
+# n = int(input()) #2 
+
+# variable = [[x,y,z] for num in n for num2 in [x,y,z]]
+
+# # Print the below string in reverse order.
+
+# from pytube import YouTube
+# youtube = YouTube(url='https://www.youtube.com/watch?v=wfN_U_-8AVk')
+
+# enumarate_list = enumerate(youtube.streams.filter(progressive=True))
+# for num , stream in enumarate_list:
+#     print(num, stream)
+
+# download = int(input('Which one you would like to download?: '))
+
+# youtube.streams.filter(progressive=True)[download].download()
+
+# 🎋🎋🎋🎋🎋🎋🎋🎋🎋🎋🎋🎋🎋🎋🎋🎋🎋🎋🎋🎋🎋🎋🎋🎋🎋🎋🎋🎋🎋🎋🎋🎋🎋🎋
+# Using seedr api
+
+# Import the lib
+# from seedr import SeedrAPI
+
+# # Create API client
+# seedr = SeedrAPI(email="asadjani202@gmail.com", password="ak91249124")
+
+# # Add a torrent
+# output = seedr.add_torrent("https://www.1377x.to/torrent/3286747/UDEMY-CRASH-COURSE-ELECTRONICS-AND-PCB-DESIGN-FTU/#")
+
+# print(output)
+
+
+
